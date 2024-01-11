81e45f8fbd0052a7f82ef56861731cf9582fde50
yasmenalmajdoubah <yasmen.almajdoubah@gmail.com>
2024-01-10 19:21:44 -0800

    coponents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
-
+import React, { useState, createContext } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+export const userContext = createContext();
 function App() {
   return (
-    <div className="App">
+    <userContext.Provider>
+       <div className="App">
       <h1>Hello, World!</h1>
+      <Routes>
+          <Route path="/users/register" element={<Register />} />
+          <Route path="/users/login" element={<Login />} />
+
+          
+        </Routes>
     </div>
+    </userContext.Provider>
+   
   );
 }
 
